fce6e7719cf17bc7cfc34f64ac076dbb7a8cd635
David Mbaimbai <dmbaimbai@csod.com>
2025-01-30 23:07:37 +0200

    New UI for the Project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseAPIUrl: 'http://localhost:9292/api/v1',
+  baseAPIUrl: 'https://mysql-ng8g-production.up.railway.app/api/v1',
   appName: ' VerifyInfluencers',
   version: '1.0.0',
 };
